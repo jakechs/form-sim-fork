68fd033cd63b415f0355b33d7072cecb98ba52a1
priyank.mathur@gmail.com <priyank.mathur@gmail.com>
2013-07-28 23:19:39 -0700

    files saved in db

--- compare.js
+++ compare.js
@@ -4,17 +4,23 @@ if (typeof process == 'undefined' || process.argv[0] != "node") {
 	throw new Error("This code must be run on server side under NodeJS");
 }
 
+var argv = require('optimist')
+			.default('m','1')
+			.default('i','img/tax.jpg')
+			.default('o','result.txt')
+			.argv;
+
 //!!! Please provide your application id and password and remove this line !!!
 // Name of application you created
 var appId = 'Document similarity for uploaded forms';
 // Password should be sent to your e-mail after application was created
 var password = 'gXYVNyQgHTHqGLNBOkWSITCH';
 
-var imagePath = 'tax.jpg';
-var outputPath = 'result.txt';
+var imagePath = argv.i;
+var outputPath = argv.o;
+var master, docs;
 
 try {
-	console.log("ABBYY Cloud OCR SDK Sample for Node.js");
 
 	var ocrsdkModule = require('./ocrsdk.js');
 	var ocrsdk = ocrsdkModule.create(appId, password);
@@ -71,20 +77,87 @@ try {
 		ocrsdk.waitForCompletion(taskData.id, processingCompleted);
 	}
 
+	function getID (idf) {
+		var max = -9999;
+
+		if (Object.keys(idf).length <= 0)
+			max = 0;
+		else {
+			for (var k in idf) {
+				if(max < idf[k].id) {
+					max = idf[k].id;
+				}
+			}
+		}		
+
+		return max + 1;
+	};
+
+	function addDocToDB(doc, idf) {
+		var master_idf = idf || {};
+		var tokens = doc.tokens;
+		var newTokens = {};
+
+		Object.keys(tokens).forEach(function (el) {
+			if(master_idf.hasOwnProperty(el)) {
+				master_idf[el].cnt = master_idf[el].cnt + tokens[el];
+				newTokens[master_idf[el].id] = tokens[el];
+			}
+			else {
+				master_idf[el] = {id:getID(master_idf)};
+				master_idf[el].cnt =	 tokens[el];
+				newTokens[master_idf[el].id] = tokens[el];
+			}
+		});
+
+		doc.tokens = newTokens;
+		delete master_idf.serializeFunctions;
+
+		console.log(JSON.stringify(master_idf));
+		console.log(JSON.stringify(doc));
+
+		docs.insert(doc);
+		master.update({}, master_idf, {upsert:true});
+	};
+
 	var settings = new ocrsdkModule.ProcessingSettings();
 	// Set your own recognition language and output format here
 	settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
 	settings.exportFormat = "txt"; // All possible values are listed in 'exportFormat' parameter description 
                                    // at http://ocrsdk.com/documentation/apireference/processImage/
 
-	console.log("Uploading image..");
+	//console.log("Uploading image..");
 	//ocrsdk.processImage(imagePath, settings, uploadCompleted);
 
-	var wp = require('./wordProc');
-	wp.init('./result.txt');
-	var c = wp.tokenize();
-	console.log(c);
+	var MongoConnection = require('./MongoConnection');
+	new MongoConnection("similarity", function (err, mc) {
+		if(err)
+			console.log(err);
 
+		var wp = require('./wordProc');
+		wp.init(outputPath);
+
+		mc.getCollection([wp.master, "docs"], function (collections) {
+			master = collections.collection[0];
+			docs = collections.collection[1];
+
+			var newdoc = wp.getDetails( wp.tokenize());
+			newdoc.file = imagePath;
+			//console.log(newdoc);
+
+			master.find({}).toArray(function (err, items) {
+				var idf = items[0];
+				if (argv.m == '1') {
+					console.log("Adding new doc");
+					addDocToDB(newdoc, idf);
+				}
+				else if (argv.m == '2') {
+					console.log("Comparing doc");
+				}
+				mc.db.close();
+			});
+		});
+	});
 
 } catch (err) {
 	console.log("Error: " + err.message);
--- wordProc.js
+++ wordProc.js
@@ -2,20 +2,36 @@ var natural = require('natural');
 var fs = require('fs');
 
 function wordProc (file) {
-	console.log('Creating word proc');
 }
 
 wordProc.prototype.init = function(file)
 {
-	console.log('Init word proc');
-	this.master_idf = "master_idf";
+	this.master = "master_idf";
 	this.file = file;
-}
+};
 
 wordProc.prototype.tokenize = function () {
 	tokenizer = new natural.WordTokenizer();
 	return tokenizer.tokenize(fs.readFileSync(this.file).toString());
 };
+
+wordProc.prototype.getDetails = function(data) {
+	var response = {}
+	response.size = data.length;
+	var tokens = {};
+
+	data.forEach(function (el) {
+		if(tokens.hasOwnProperty(el))
+			tokens[el] = tokens[el] + 1;
+		else
+			tokens[el] = 1;
+	});
+
+	response.tokens = tokens;
+	response.vocabsize = Object.keys(tokens).length;
+
+	return response;
+}
 
 module.exports = new wordProc();
 
